ff4ea65c99592a6b505899f5e6d7a30798fe4bcb
pyama <www.kazu.com@gmail.com>
2026-04-28 21:44:04 +0900

    修正(confluence): <li>内の<br>を除去してFabric Editor 400エラーを修正
    Confluence Fabric EditorのADFはlistItem内のhardBreak(=<br>)を
    非サポートのため、sanitize後に<li>末尾の<br>を除去する。
    またtarget属性生成元のHrefTargetBlankフラグも不要のため削除した。

--- domain/repository/confluence_repository.go
+++ domain/repository/confluence_repository.go
@@ -4,12 +4,16 @@ import (
 	"context"
 	"fmt"
 	"os"
+	"regexp"
 
 	"github.com/microcosm-cc/bluemonday"
 	"github.com/pyama86/YAS3/domain/entity"
 	"github.com/russross/blackfriday/v2"
 	goconfluence "github.com/virtomize/confluence-go-api"
 )
+
+// Confluence Fabric EditorのADFはlistItem内のhardBreakを非サポートのため除去する
+var brInListItem = regexp.MustCompile(`(?i)<br\s*/?>\s*</li>`)
 
 type ConfluenceRepository struct {
 	ansectorID string
@@ -38,11 +42,11 @@ func NewConfluenceRepository(domain, user, password, spaceKey, ancestorID string
 }
 
 func (c *ConfluenceRepository) ExportPostMortem(ctx context.Context, title, body string, service *entity.Service) (string, error) {
-	renderer := blackfriday.NewHTMLRenderer(blackfriday.HTMLRendererParameters{
-		Flags: blackfriday.HrefTargetBlank,
-	})
+	// HrefTargetBlankは使用しない（target属性はConfluence Storage Formatで非サポート）
+	renderer := blackfriday.NewHTMLRenderer(blackfriday.HTMLRendererParameters{})
 	output := blackfriday.Run([]byte(body), blackfriday.WithExtensions(blackfriday.HardLineBreak+blackfriday.Autolink), blackfriday.WithRenderer(renderer))
-	html := bluemonday.UGCPolicy().SanitizeBytes(output)
+	sanitized := bluemonday.UGCPolicy().SanitizeBytes(output)
+	html := brInListItem.ReplaceAllString(string(sanitized), "</li>")
 
 	// サービスのConfluence設定があれば使用し、なければデフォルト設定を使用
 	spaceKey := c.spaceKey
